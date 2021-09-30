ae734dc98f0efbee5b0763602406db9ec1a6ff25
Svengeelhoed <99069588@mydavinci.nl>
2021-09-30 11:41:36 +0200

    tri-tryout3 verbeterd

--- tri-tryout3.py
+++ tri-tryout3.py
@@ -1,11 +1,14 @@
 def gamering(naam, leeftijd):
-    print("Hallo, " + naam + " uw leeftijd is " + leeftijd + " jaar.")
+    print("Hallo " + naam + ", uw leeftijd is " + leeftijd + " jaar.")
 
 def start():
     naam = input("Wat is uw naam? ")
+    if naam == "stop":
+        exit()
     leeftijd = input("Hoe oud bent u? ")
+    if leeftijd == "stop":
+        exit()
     gamering(naam,leeftijd)
     start()
 
-if start() == True:
-    start()
+start()
